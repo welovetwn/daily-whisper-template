5e2a6df403dd8fda0cc1687dbef6e2555738c341
welovetwn <welovetwn@gmail.com>
2026-04-06 15:50:28 +0800

    更新背景圖片連結

--- js/app.js
+++ js/app.js
@@ -4,21 +4,21 @@ const images = [
   // Mountains & Lakes
   "https://images.unsplash.com/photo-1501785888041-af3ef285b470",
   "https://images.unsplash.com/photo-1500530855697-b586d89ba3ee",
-  "https://images.unsplash.com/photo-1493244040629-496f6d136cc3",
+  "https://images.unsplash.com/photo-1604440095301-4ec2f9230155",
   "https://images.pexels.com/photos/417173/pexels-photo-417173.jpeg",
   "https://images.pexels.com/photos/355241/pexels-photo-355241.jpeg",
   "https://images.pexels.com/photos/326278/pexels-photo-326278.jpeg",
-  "https://images.pexels.com/photos/349758/pexels-photo-349758.jpeg",
-  "https://images.pexels.com/photos/548463/pexels-photo-548463.jpeg",
+  "https://images.pexels.com/photos/23973445/pexels-photo-23973445.jpeg",
+  "https://images.pexels.com/photos/13873466/pexels-photo-13873466.jpeg",
 
   // Forest & Trees
   "https://images.unsplash.com/photo-1441974231531-c6227db76b6e",
   "https://images.unsplash.com/photo-1506765515384-028b60a970df",
-  "https://images.pexels.com/photos/4827/nature-forest-industry-rails.jpg",
+  "https://images.pexels.com/photos/33279929/pexels-photo-33279929.jpeg",
   "https://images.pexels.com/photos/164008/pexels-photo-164008.jpeg",
   "https://images.pexels.com/photos/547114/pexels-photo-547114.jpeg",
   "https://images.pexels.com/photos/115634/pexels-photo-115634.jpeg",
-  "https://images.pexels.com/photos/104827/cathedral-forest-path-nature-104827.jpeg",
+  "https://images.pexels.com/photos/6406918/pexels-photo-6406918.jpeg",
   "https://images.pexels.com/photos/459225/pexels-photo-459225.jpeg",
 
   // Ocean & Beach
@@ -28,24 +28,24 @@ const images = [
   "https://images.pexels.com/photos/533923/pexels-photo-533923.jpeg",
   "https://images.pexels.com/photos/1450357/pexels-photo-1450357.jpeg",
   "https://images.pexels.com/photos/187962/pexels-photo-187962.jpeg",
-  "https://images.pexels.com/photos/918774/pexels-photo-918774.jpeg",
+  "https://images.pexels.com/photos/1001682/pexels-photo-1001682.jpeg",
   "https://images.pexels.com/photos/457882/pexels-photo-457882.jpeg",
 
   // Sunset & Sunrise
   "https://images.unsplash.com/photo-1518837695005-2083093ee35b",
-  "https://images.unsplash.com/photo-1529034940185-bb0f3f1f78e0",
+  "https://images.pexels.com/photos/34021102/pexels-photo-34021102.jpeg",
   "https://images.pexels.com/photos/462353/pexels-photo-462353.jpeg",
   "https://images.pexels.com/photos/417173/pexels-photo-417173.jpeg",
   "https://images.pexels.com/photos/287158/pexels-photo-287158.jpeg",
   "https://images.pexels.com/photos/462118/pexels-photo-462118.jpeg",
-  "https://images.pexels.com/photos/417629/pexels-photo-417629.jpeg",
+  "https://images.pexels.com/photos/35981637/pexels-photo-35981637.jpeg",
   "https://images.pexels.com/photos/247599/pexels-photo-247599.jpeg",
 
   // Scenery & Nature
   "https://images.unsplash.com/photo-1500530855697-b586d89ba3ee",
-  "https://images.unsplash.com/photo-1493244040629-496f6d136cc3",
+  "https://images.pexels.com/photos/34625026/pexels-photo-34625026.jpeg",
   "https://images.unsplash.com/photo-1500534623283-312aade485b7",
-  "https://images.pexels.com/photos/349758/pexels-photo-349758.jpeg",
+  "https://images.pexels.com/photos/27022164/pexels-photo-27022164.jpeg",
   "https://images.pexels.com/photos/414171/pexels-photo-414171.jpeg",
   "https://images.pexels.com/photos/672142/pexels-photo-672142.jpeg",
   "https://images.pexels.com/photos/2559941/pexels-photo-2559941.jpeg",
@@ -54,41 +54,41 @@ const images = [
   // Clouds & Skies
   "https://images.unsplash.com/photo-1500530855697-b586d89ba3ee",
   "https://images.unsplash.com/photo-1448375240586-882707db888b",
-  "https://images.pexels.com/photos/349758/pexels-photo-349758.jpeg",
+  "https://images.pexels.com/photos/9854061/pexels-photo-9854061.jpeg",
   "https://images.pexels.com/photos/414171/pexels-photo-414171.jpeg",
   "https://images.pexels.com/photos/531880/pexels-photo-531880.jpeg",
   "https://images.pexels.com/photos/1549277/pexels-photo-1549277.jpeg",
-  "https://images.pexels.com/photos/946632/pexels-photo-946632.jpeg",
+  "https://images.pexels.com/photos/13768451/pexels-photo-13768451.jpeg",
   "https://images.pexels.com/photos/1287147/pexels-photo-1287147.jpeg",
 
   // Mountains Extended
   "https://images.pexels.com/photos/3408744/pexels-photo-3408744.jpeg",
-  "https://images.pexels.com/photos/1252892/pexels-photo-1252892.jpeg",
+  "https://images.pexels.com/photos/14672474/pexels-photo-14672474.jpeg",
   "https://images.pexels.com/photos/215687/pexels-photo-215687.jpeg",
-  "https://images.pexels.com/photos/552581/pexels-photo-552581.jpeg",
+  "https://images.pexels.com/photos/35900181/pexels-photo-35900181.jpeg",
   "https://images.pexels.com/photos/417173/pexels-photo-417173.jpeg",
   "https://images.pexels.com/photos/422088/pexels-photo-422088.jpeg",
-  "https://images.pexels.com/photos/3015995/pexels-photo-3015995.jpeg",
+  "https://images.pexels.com/photos/17881053/pexels-photo-17881053.jpeg",
   "https://images.pexels.com/photos/277667/pexels-photo-277667.jpeg",
 
   // More scenic variety
   "https://images.pexels.com/photos/1001682/pexels-photo-1001682.jpeg",
   "https://images.pexels.com/photos/1410232/pexels-photo-1410232.jpeg",
-  "https://images.pexels.com/photos/158607/cairn-fog-sky-rocks-158607.jpeg",
+  "https://images.pexels.com/photos/29222109/pexels-photo-29222109.jpeg",
   "https://images.pexels.com/photos/1888908/pexels-photo-1888908.jpeg",
-  "https://images.pexels.com/photos/2360478/pexels-photo-2360478.jpeg",
+  "https://images.unsplash.com/photo-1775049618716-b6edef32daff",
   "https://images.pexels.com/photos/459225/pexels-photo-459225.jpeg",
   "https://images.pexels.com/photos/347140/pexels-photo-347140.jpeg",
   "https://images.pexels.com/photos/355241/pexels-photo-355241.jpeg",
 
   // Even more landscapes
-  "https://images.pexels.com/photos/3015995/pexels-photo-3015995.jpeg",
+  "https://images.pexels.com/photos/17881053/pexels-photo-17881053.jpeg",
   "https://images.pexels.com/photos/2559941/pexels-photo-2559941.jpeg",
   "https://images.pexels.com/photos/247599/pexels-photo-247599.jpeg",
-  "https://images.pexels.com/photos/1722211/pexels-photo-1722211.jpeg",
+  "https://images.pexels.com/photos/34625026/pexels-photo-34625026.jpeg",
   "https://images.pexels.com/photos/1770809/pexels-photo-1770809.jpeg",
-  "https://images.pexels.com/photos/263503/pexels-photo-263503.jpeg",
-  "https://images.pexels.com/photos/391679/pexels-photo-391679.jpeg",
+  "https://images.pexels.com/photos/7694761/pexels-photo-7694761.jpeg",
+  "https://images.pexels.com/photos/8544386/pexels-photo-8544386.jpeg",
   "https://images.pexels.com/photos/302769/pexels-photo-302769.jpeg"
 ];
 function randomImg(){ return images[Math.floor(Math.random()*images.length)]; }
